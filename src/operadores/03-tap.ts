12ea549db9bb6824edf64fb5272d1d6207265755
Donaldo Quintana <donaldoquintana@MacBook-Air-de-Donaldo.local>
2020-06-15 19:37:11 -0500

    Actualizando proyecto

---
+++ src/operadores/03-tap.ts
@@ -0,0 +1,15 @@
+import { range, from, fromEvent } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
+
+const numeros$ = range(1, 5);
+numeros$.pipe(
+    tap(x => {
+        console.log('antes', x);
+        return 100;
+    }),
+    map(val => val * 10),
+    tap({
+        next: valor => console.log('despues', valor),
+        complete: () => console.log('completado')
+    })
+).subscribe(val => console.log('subs',val));
